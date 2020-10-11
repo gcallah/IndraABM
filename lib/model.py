a59dae7843fae1215e893cd9b5738c78eb8aa48a
gcallah <gcallah@mac.com>
2020-10-11 04:15:02 -0400

    Basic model kinda working.

--- lib/model.py
+++ lib/model.py
@@ -23,6 +23,7 @@ class Model():
         self.props = init_props(self.name, props)
         self.user_type = os.getenv("user_type", TERMINAL)
         self.create_user()
+        self.groups = []
         self.groups = self.create_groups()
         self.env = self.create_env()
 
--- models/basic.py
+++ models/basic.py
@@ -6,6 +6,9 @@ do nothing except move around randomly.
 """
 
 from lib.model import Model
+from indra.composite import Composite
+from indra.display_methods import RED, BLUE
+
 
 MODEL_NAME = "basic"
 
@@ -16,10 +19,20 @@ class Basic(Model):
     some agents that move around, and allows us to test if
     the system as a whole is working.
     """
+    def __init__(self, name=MODEL_NAME):
+        super().__init__(name)
+
+    def create_groups(self):
+        self.groups.append(Composite("Blues", {"color": BLUE}))
+        self.groups.append(Composite("Reds", {"color": RED}))
+
+    def run(self):
+        super().run()
+        print("My groups are:", self.groups)
 
 
 def main():
-    model = Basic(MODEL_NAME)
+    model = Basic()
     model.run()
     return 0
 
