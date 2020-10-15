d67ec7099bb0fb009e59b02a66d5c488db3f65ae
gcallah <gcallah@mac.com>
2020-10-15 18:28:32 -0400

    Creating tests for registry.

---
+++ registry/tests/test_agent_registry.py
@@ -0,0 +1,30 @@
+"""
+This is the test suite for agent_registry.py.
+"""
+
+from unittest import TestCase, main
+
+from registry.agent_registry import get_agent, register_agent, del_agent
+
+
+class AgentRegistryTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_get_agent(self):
+        """
+        See if we get an agent we have registered back.
+        """
+
+    def test_register_agent(self):
+        """
+        Test registering an agent.
+        """
+
+    def test_del_agent(self):
+        """
+        Test deleting an agent.
+        """
