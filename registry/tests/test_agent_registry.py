b97abb3a959648c19f57bd7b6f62d304a23f3c1c
gcallah <gcallah@mac.com>
2020-10-15 19:11:49 -0400

    Simple agent registry created with all tests passing.

--- registry/tests/test_agent_registry.py
+++ registry/tests/test_agent_registry.py
@@ -4,7 +4,11 @@ This is the test suite for agent_registry.py.
 
 from unittest import TestCase, main
 
-from registry.agent_registry import get_agent, reg_agent, del_agent
+from lib.agent import Agent
+from registry.agent_registry import get_agent, reg_agent
+from registry.agent_registry import del_agent, agent_reg
+
+TEST_AGENT_NM = "Test agent"
 
 
 class AgentRegistryTestCase(TestCase):
@@ -18,13 +22,22 @@ class AgentRegistryTestCase(TestCase):
         """
         See if we get an agent we have registered back.
         """
+        agent_to_reg = Agent(TEST_AGENT_NM)
+        reg_agent(TEST_AGENT_NM, agent_to_reg)
+        self.assertEquals(agent_to_reg, get_agent(TEST_AGENT_NM))
 
-    def test_register_agent(self):
+
+    def test_reg_agent(self):
         """
         Test registering an agent.
         """
+        reg_agent(TEST_AGENT_NM, Agent(TEST_AGENT_NM))
+        self.assertIn(TEST_AGENT_NM, agent_reg)
 
     def test_del_agent(self):
         """
         Test deleting an agent.
         """
+        reg_agent(TEST_AGENT_NM, Agent(TEST_AGENT_NM))
+        del_agent(TEST_AGENT_NM)
+        self.assertNotIn(TEST_AGENT_NM, agent_reg)
