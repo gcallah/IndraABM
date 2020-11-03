cff5af64b5c612b8553469068a89ebbccf01c331
Denys Fenchenko <denisfench@gmail.com>
2020-11-03 17:22:28 -0500

    Updates to the panic model

--- v2models/panic.py
+++ v2models/panic.py
@@ -6,7 +6,6 @@ from lib.agent import Agent, DONT_MOVE, switch
 from lib.group import Group
 from lib.space import neighbor_ratio
 from lib.display_methods import RED, GREEN
-from lib.display_methods import TREE
 from lib.env import Env
 from lib.model import Model, create_agent
 from lib.utils import init_props
@@ -32,39 +31,6 @@ PANIC = "Panic"
 # convert to int when we need 'em that way
 CM = "0"
 PN = "1"
-
-
-PANIC_GRP_STRUCT = {
-    "calm_group": {
-        "mbr_creator": create_agent,
-        "mbr_action": agent_action,
-        "num_members": DEF_NUM_PEOPLE,
-        "color": GREEN
-    },
-    "panic_group": {
-        "mbr_creator": create_agent,
-        "mbr_action": agent_action,
-        "num_members": DEF_NUM_PANIC,
-        "color": RED
-    },
-}
-
-class Panic(Model):
-
-    def __init__(self, name=MODEL_NAME):
-        super().__init__(name, grp_struct=PANIC_GRP_STRUCT)
-
-    def run(self):
-        print("My groups are:", self.groups)
-        return super().run()
-
-def main():
-    model = Panic()
-    model.run()
-    return 0
-
-if __name__ == "__main__":
-    main()
 
 
 def is_calm(agent, *args):
@@ -98,7 +64,7 @@ def agent_action(agent, **kwargs):
         agent.add_switch(agent, CALM, PANIC)
     return DONT_MOVE
 
-# How do I get grid width and grid height from the props ?
+# What about the agent placement? Is it still working the same?
 
 def place_agent(agent, state=CM, **kwargs):
     """
@@ -142,35 +108,33 @@ def place_agent(agent, state=CM, **kwargs):
     return agent
 
 
-def set_up(props=None):
-    """
-    A func to set up a  run that can also be used by test code.
-    """
-    init_props(MODEL_NAME, props)
+panic_grps = {
+    "calm_group": {
+        "mbr_creator": create_agent,
+        "grp_action": None,
+        "mbr_action": agent_action,
+        "num_mbrs": DEF_NUM_PEOPLE,
+        "num_mbs_prop": "num_calm",
+        "color": GREEN
+    },
+    "panic_group": {
+        "mbr_creator": create_agent,
+        "grp_action": None,
+        "mbr_action": agent_action,
+        "num_members": DEF_NUM_PANIC,
+        "num_mbrs_prop": "num_panic",
+        "color": RED
+    },
+}
 
-    execution_key = int(props[EXEC_KEY].val) \
-        if props is not None else CLI_EXEC_KEY
 
-    grid_height = get_prop('grid_height', DEF_DIM,
-                           execution_key=execution_key)
-    grid_width = get_prop('grid_width', DEF_DIM, execution_key=execution_key)
-    per_panic = get_prop('per_panic', DEF_PANIC, execution_key=execution_key)
-    per_panic = per_panic/100
-    groups = []
-    calm = Composite(CALM, {"color": GREEN, "marker": TREE},
-                     execution_key=execution_key)
-    groups.append(calm)
-    panic = Composite(PANIC, {"color": RED, "marker": TREE},
-                      execution_key=execution_key)
-    groups.append(panic)
+class Panic(Model):
 
-    Env(MODEL_NAME, height=grid_height,
-        width=grid_width, members=groups,
-        execution_key=execution_key)
 
-    
-    # whereas these settings must be re-done every API re-load:
-    set_env_attr(execution_key, CLI_EXEC_KEY)
+def main():
+    model = Panic(MODEL_NAME, grp_struct=panic_grps)
+    model.run()
+    return 0
 
 
 if __name__ == "__main__":
