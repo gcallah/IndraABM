27485d0fec4c51d44a9d941ec71bfb27e439a963
gcallah <gcallah@mac.com>
2020-11-11 23:20:13 -0500

    Added tests for ff healhty and onfire functions.

--- models/tests/test_forest_fire.py
+++ models/tests/test_forest_fire.py
@@ -5,19 +5,22 @@ This is the test suite for forest_fire.py.
 from unittest import TestCase, skip
 
 from lib.agent import DONT_MOVE
-from models.forest_fire import ForestFire, main, MODEL_NAME, ff_grps
+from models.forest_fire import ForestFire, main, MODEL_NAME, ff_grps, OF
 from models.forest_fire import tree_action, plant_tree, is_healthy, is_on_fire
 
 
 class ForestFireTestCase(TestCase):
     def setUp(self):
         self.ff = ForestFire(MODEL_NAME, grp_struct=ff_grps)
-        self.htree = plant_tree("tree", 1,
+        self.htree = plant_tree("htree", 1,
                                 exec_key=self.ff.exec_key)
+        self.oftree = plant_tree("oftree", 1, state=OF,
+                                 exec_key=self.ff.exec_key)
 
     def tearDown(self):
         self.ff = None
         self.htree = None
+        self.oftree = None
 
     def test_tree_action(self):
         """
@@ -30,13 +33,15 @@ class ForestFireTestCase(TestCase):
         Checking whether the state is healthy or not
         """
         self.assertTrue(is_healthy(self.htree))
+        self.assertFalse(is_healthy(self.oftree))
 
 
     def test_is_on_fire(self):
         """
         Checking whether the state is on fire or not
         """
-        pass
+        self.assertTrue(is_on_fire(self.oftree))
+        self.assertFalse(is_on_fire(self.htree))
 
     def test_run(self):
         """
