6b71fc6356ff227b0ad0937c5bc68c857c48eeb5
Denys Fenchenko <denisfench@gmail.com>
2020-11-15 09:28:04 -0500

    chnages to panic and panic tests

--- models/panic.py
+++ models/panic.py
@@ -2,7 +2,7 @@
 A model to simulate the spread of panic in a crowd.
 """
 
-from lib.agent import DONT_MOVE, Agent
+from lib.agent import DONT_MOVE
 from lib.space import neighbor_ratio
 from lib.display_methods import RED, GREEN
 from lib.model import Model
@@ -18,55 +18,34 @@ DEF_NUM_PANIC = int(.3 * DEF_NUM_PEOPLE)
 DEF_NUM_CALM = int(.7 * DEF_NUM_PEOPLE)
 
 AGENT_PREFIX = "Agent"
-THRESHHOLD = 2
+THRESHHOLD = .2
 
 CALM = "Calm"
 PANIC = "Panic"
-
-
-def is_calm(agent, *args):
-    """
-    Checking whether the agent is calm.
-    """
-    return agent.prim_group_nm() == CALM
-
-
-def is_panicking(agent, *args):
-    """
-    Checking whether the agent is panicking.
-    """
-    return agent.prim_group_nm() == PANIC
 
 
 def agent_action(agent, **kwargs):
     """
     This is what agents do each turn of the model.
     """
-    ratio = neighbor_ratio(agent, pred_one=is_calm, pred_two=is_panicking)
+    ratio = neighbor_ratio(agent, lambda agent: agent.group_name() == PANIC)
     if ratio > THRESHHOLD:
         if DEBUG2:
             print("Changing the agent's group to panic!")
         agent.has_acted = True
-        print("Agent's name is", agent)
-        get_model(agent.exec_key).add_switch(str(agent), CALM, PANIC)
+        if agent.group_name() == CALM:
+            get_model(agent.exec_key).add_switch(str(agent), CALM, PANIC)
     return DONT_MOVE
-
-
-def create_pagent(name, i, action=None, exec_key=None):
-    return Agent(name + str(i),
-                 exec_key=exec_key, action=action)
 
 
 panic_grps = {
     CALM: {
-        # "mbr_creator": create_pagent,
         "grp_action": None,
         "mbr_action": agent_action,
         "num_mbrs": DEF_NUM_CALM,
         "color": GREEN
     },
     PANIC: {
-        # "mbr_creator": create_pagent,
         "grp_action": None,
         "mbr_action": agent_action,
         "num_mbrs": DEF_NUM_PANIC,
--- models/tests/test_panic.py
+++ models/tests/test_panic.py
@@ -3,50 +3,30 @@ This is the test suite for panic.py.
 """
 
 from unittest import TestCase, skip  # , main
-
 from lib.agent import DONT_MOVE
-
+from lib.agent import Agent
 from models.panic import Panic, main, MODEL_NAME, panic_grps, agent_action
-from models.panic import create_pagent, is_calm, is_panicking
 
 
 class PanicTestCase(TestCase):
     def setUp(self):
         self.panic = Panic(MODEL_NAME, grp_struct=panic_grps)
-        self.calm_agent = create_pagent("calm", 1,
-                                        exec_key=self.panic.exec_key)
-        # print(self.calm_agent["state"])
-        self.panic_agent = create_pagent("panic", 1,
-                                         exec_key=self.panic.exec_key)
-
+        self.calm_agent = Agent(name="calm",
+                                     exec_key=self.panic.exec_key)
+        self.panic_agent = Agent(name="panic",
+                                     exec_key=self.panic.exec_key)
 
     def tearDown(self):
         self.panic = None
-        self.calm_agent = None
-        self.panic_agent = None
-
-
-    '''
-    def test_is_calm(self):
-        self.assertTrue(is_calm(self.calm_agent))
-        self.assertFalse(is_calm(self.panic_agent))
-
-    def test_is_panicking(self):
-        self.assertTrue(is_panicking(self.panic_agent))
-        self.assertFalse(is_panicking(self.calm_agent))
-    '''
-
 
     def test_agent_action(self):
         self.assertEqual(DONT_MOVE, agent_action(self.calm_agent))
-
 
     def test_run(self):
         """
         Does running the model work? (return of 0)
         """
         self.assertEqual(0, self.panic.run())
-
 
     def test_main(self):
         """
