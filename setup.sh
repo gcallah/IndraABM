ba46c2e5965cf38b882afed5f7e8c6a29cf00466
gcallah <gcallah@mac.com>
2020-11-16 21:56:47 -0500

    Fixed bad requirements dir.

--- setup.sh
+++ setup.sh
@@ -6,4 +6,4 @@ echo "export PYTHONPATH=$wd:$PYTHONPATH" >> $HOME/$1
 export PYTHONPATH=$wd:$PYTHONPATH
 echo "export INDRA_HOME=$wd" >> $HOME/$1
 export INDRA_HOME=$wd
-pip3 install -r docker/requirements-dev.txt
+pip3 install -r requirements/requirements-dev.txt
