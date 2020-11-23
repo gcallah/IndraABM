6058d4dc83a325a150c49e9991ef446053094706
Amadou Diallo <aod259@.nyu.edu>
2020-11-23 16:07:17 -0500

    added test_drinter_action and env set up

--- models/tests/test_el_farol.py
+++ models/tests/test_el_farol.py
@@ -4,10 +4,10 @@ This is the test suite for el_farol.py.
 
 from unittest import TestCase, main,skip
 
-
+from lib.agent import Agent,MOVE,DONT_MOVE
 import models.el_farol as el_farol
 from models.el_farol import AT_BAR, AT_HOME, MOTIV, MODEL_NAME
-from models.el_farol import ElFarol, el_farol_grps
+from models.el_farol import ElFarol, el_farol_grps, drinker_action
 
 def header(s):
     print("\n==================")
@@ -18,38 +18,29 @@ class ElFarolTestCase(TestCase):
     
     def setUp(self):
         header("Setting up")
-        groups = []
-        self.ef = ElFarol(MODEL_NAME, grp_struct=el_farol_grps)
         #create an evirement for testing and get exect key
-
+        self.ef = ElFarol(MODEL_NAME, grp_struct=el_farol_grps)
+        self.drinker = Agent(name = "drinker", exec_key = self.ef.exec_key)
+        
     def tearDown(self):
         """
         This should undo what setup() does!
         """
         header("Tearing Down")
         self.bob = None
-    def test_main(self):
-        self.assertEqual(el_farol.main(), 0)
+    def test_drinker_action(self):
+            """
+            Test drinker action
+            """
+            return self.assertEqual(MOVE,drinker_action(self.drinker))
 
-    @skip("Not implimented yet")
-    def test_create_non_drinker(self):
-        """
-        Test creating a non-drinker.
-        """
-        agent = el_farol.create_non_drinker(DRINKERS, 0)
-        self.assertIn(MOTIV, agent)
-
-    @skip("Not implimented yet")
-    def test_create_drinker(self):
-        """
-        Test creating a drinker.
-        """
-        agent = el_farol.create_drinker(NON_DRINKERS, 0)
-        self.assertIn(MOTIV, agent)
     @skip("Not implimented yet")
     def test_discourage(self):
         discouraged = el_farol.discourage(1)
         self.assertEqual(discouraged, 1)
+
+    def test_main(self):
+        self.assertEqual(el_farol.main(), 0)
 
     if __name__ == '__main__':
         main()
