bdcbd502e60a600670beff8dd9fd62b394850759
Denys Fenchenko <denisfench@gmail.com>
2020-12-11 17:19:00 -0500

    Adding a script to deploy on PythonAnywhere

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # This shell file deploys a new version to our server.
 
-eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 ~/.ssh/id_rsa # Allow read access to the private key
-ssh-add ~/.ssh/id_rsa # Add the private key to SSH
+export project_name=IndraABM
 
 echo "SSHing to PythonAnywhere."
-ssh -o "StrictHostKeyChecking no" indrasnet@ssh.pythonanywhere.com << EOF
-    cd ~/indras_net; ~/indras_net/rebuild.sh
+sshpass -p $1 ssh -o "StrictHostKeyChecking no" $project_name@ssh.pythonanywhere.com << EOF
+    cd ~/$project_name; ~/$project_name/rebuild.sh $2
 EOF
