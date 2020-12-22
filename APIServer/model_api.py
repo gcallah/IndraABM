342e3f200781fecdbbfd79dfccecbd0f38e2430c
Denys Fenchenko <denisfench@gmail.com>
2020-12-21 22:36:21 -0500

    Enabling the creation of the model with the correct  name

--- APIServer/model_api.py
+++ APIServer/model_api.py
@@ -2,13 +2,15 @@
 This module restores an env from json and runs it.
 """
 from lib.model import Model
+from APIServer.models_api import get_model
 
 
 def create_model(model_id, props, indra_dir):
     """
     We get some props and create a model in response.
     """
-    return Model("test_model", props=props)
+    model = get_model(model_id, indra_dir=indra_dir)
+    return Model(model["name"], props=props)
 
 
 def run_model(serial_model, run_time):
