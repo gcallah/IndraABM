1951ef16d128ef1ad93eb697cf7757289f7c2e1c
Denys Fenchenko <denisfench@gmail.com>
2020-12-23 19:37:03 -0500

    Implementing the run of the correct model

--- APIServer/model_api.py
+++ APIServer/model_api.py
@@ -2,9 +2,9 @@
 This module restores an env from json and runs it.
 """
 import importlib
-
 from lib.model import Model
-from APIServer.models_api import get_model
+from APIServer.api_utils import ERROR
+from APIServer.models_api import get_model, load_models
 
 
 def create_model(model_id, props, indra_dir):
@@ -12,6 +12,7 @@ def create_model(model_id, props, indra_dir):
     We get some props and create a model in response.
     """
     model = get_model(model_id, indra_dir=indra_dir)
+    print("The model is", model)
     mod_name = f'{model["package"]}.{model["module"]}'
     print("mod_name = ", mod_name)
     this_mod = importlib.import_module(mod_name)
@@ -19,13 +20,25 @@ def create_model(model_id, props, indra_dir):
     return model
 
 
-def run_model(serial_model, run_time):
+def run_model(serial_model, run_time, indra_dir):
     """
     We create a dummy env that fills itself in to create
     the real env from the payload.
     """
-    print("About to recreate model from json")
+    model_module = serial_model["name"]
+    print(isinstance(model_module, str))
+    print("THE  MODEL NAME IS", model_module)
+    models_db = load_models(indra_dir)
+    print("THE MODEL DB IS", models_db)
+    for model in models_db:
+        if model["module"] == model_module:
+            print("The found model is", model["module"])
+            mod_file = f'{model["package"]}.{model["module"]}'
+            print("The model file is", mod_file)
+            this_mod = importlib.import_module(mod_file)
+            model = this_mod.create_model(serial_obj=serial_model)
+            model.runN(run_time)
+            return model
+    print(model_module + " " + "was not found, creating the default model")
     model = Model(serial_obj=serial_model)
-    print("About to run model restored from json")
-    model.runN(run_time)
     return model
