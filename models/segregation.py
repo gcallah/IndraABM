3dc47b455dff9b7b26466afe5852292731b85d02
Denys Fenchenko <denisfench@gmail.com>
2020-12-29 22:27:24 -0500

    Transitioning the  segregation model

---
+++ models/segregation.py
@@ -0,0 +1,94 @@
+"""
+A model to simulate the spread of fire in a forest.
+"""
+
+from lib.agent import DONT_MOVE
+from lib.display_methods import RED, BLUE
+from lib.model import Model
+
+MODEL_NAME = "segregation"
+DEBUG = False  # Turns debugging code on or off
+DEBUG2 = False  # Turns deeper debugging code on or off
+
+NUM_RED = 250
+NUM_BLUE = 250
+
+DEF_CITY_DIM = 40
+
+TOLERANCE = "tolerance"
+DEVIATION = "deviation"
+GRP_INDEX = "grp_index"
+
+DEF_HOOD_SIZE = 1
+DEF_TOLERANCE = .5
+DEF_SIGMA = .2
+
+MIN_TOL = 0.1
+MAX_TOL = 0.9
+
+BLUE_GRP_IDX = 0
+RED_GRP_IDX = 1
+
+HOOD_SIZE = 4
+
+NOT_ZERO = .001
+
+BLUE_AGENTS = "Blue agents"
+RED_AGENTS = "Red agents"
+
+group_names = [BLUE_AGENTS, RED_AGENTS]
+
+hood_size = None
+
+opp_group = None
+
+
+def agent_action(agent, **kwargs):
+    """
+    This is what agents do each turn of the model.
+    """
+    return DONT_MOVE
+
+
+segregation_grps = {
+    "blue_group": {
+        "grp_action": None,
+        "mbr_action": agent_action,
+        "num_mbrs": NUM_BLUE,
+        "num_mbs_prop": "num_blue",
+        "color": BLUE
+    },
+    "red_group": {
+        "grp_action": None,
+        "mbr_action": agent_action,
+        "num_members": NUM_RED,
+        "num_mbrs_prop": "num_red",
+        "color": RED
+    },
+}
+
+
+class Segregation(Model):
+    """
+    Thomas Schelling's famous model of neighborhood segregation.
+    """
+
+
+def create_model(serial_obj=None):
+    """
+    This is for the sake of the API server:
+    """
+    if serial_obj is not None:
+        return Segregation(serial_obj=serial_obj)
+    else:
+        return Segregation(MODEL_NAME, grp_struct=segregation_grps)
+
+
+def main():
+    model = create_model()
+    model.run()
+    return 0
+
+
+if __name__ == "__main__":
+    main()
