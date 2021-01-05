dd623198cc0217333beee837df30c6dc8422a468
Denys Fenchenko <denisfench@gmail.com>
2021-01-05 11:45:18 -0500

    Changes to API rebooting process

--- rebuild.sh
+++ rebuild.sh
@@ -2,12 +2,14 @@
 # This runs on the production server: fetches new code,
 # installs needed packages, and restarts the server.
 
+export project_name=IndraABM
+
 # get new source code onto the server
 git pull origin master
 # activate our virtual env:
-source /home/indrasnet/.virtualenvs/django2/bin/activate
+source /home/project_name/.virtualenvs/indra-virtualenv/bin/activate
 # install all of our packages:
 pip install -r docker/requirements.txt
 echo "Going to reboot the webserver"
-API_TOKEN=14ca851554fc716c30e031b0583fb7e64e05e0db pa_reload_webapp.py indrasnet.pythonanywhere.com
+API_TOKEN=$pa_pwd pa_reload_webapp.py project_name.pythonanywhere.com
 touch reboot
