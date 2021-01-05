e5a61891968428af6efc43911a8a1e3a8e0f4c3d
Denys Fenchenko <denisfench@gmail.com>
2021-01-05 10:45:47 -0500

    Changes to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@
 export project_name=IndraABM
 
 echo "SSHing to PythonAnywhere."
-sshpass -p $1 ssh -o StrictHostKeyChecking=no $project_name@ssh.pythonanywhere.com << EOF
+sshpass -p $1 ssh $project_name@ssh.pythonanywhere.com << EOF
     cd ~/$project_name; ~/$project_name/rebuild.sh $2
 EOF
