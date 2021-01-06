97a630ce17130245d1c6cd8f896df9ef65b5423b
Denys Fenchenko <denisfench@gmail.com>
2021-01-05 20:16:24 -0500

    Changes to rebuild shell script

--- rebuild.sh
+++ rebuild.sh
@@ -2,14 +2,12 @@
 # This runs on the production server: fetches new code,
 # installs needed packages, and restarts the server.
 
-export project_name=IndraABM
-
 # get new source code onto the server
 git pull origin master
 # activate our virtual env:
-source /home/project_name/.virtualenvs/indra-virtualenv/bin/activate
+source /home/IndraABM/.virtualenvs/indra-virtualenv/bin/activate
 # install all of our packages:
 pip install -r docker/requirements.txt
 echo "Going to reboot the webserver"
-API_TOKEN=$api_token pa_reload_webapp.py project_name.pythonanywhere.com
+API_TOKEN=$api_token pa_reload_webapp.py IndraABM.pythonanywhere.com
 touch reboot
