196e356e32f29929c2bd5be1896129dbee53e12e
gcallah <gcallah@mac.com>
2021-01-06 01:10:52 -0500

    Put in full path to reload API script.

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,8 @@
 # This runs on the production server: fetches new code,
 # installs needed packages, and restarts the server.
 
+export RELOAD=/home/IndraABM/.local/bin/pa_reload_webapp.py
+
 # get new source code onto the server
 git pull origin master
 # activate our virtual env:
@@ -9,5 +11,5 @@ source /home/IndraABM/.virtualenvs/indra-virtualenv/bin/activate
 # install all of our packages:
 pip install -r requirements/requirements.txt
 echo "Going to reboot the webserver"
-API_TOKEN=1bd1d39b21de527564c04430402fe8eae3b2825b pa_reload_webapp.py IndraABM.pythonanywhere.com
+API_TOKEN=1bd1d39b21de527564c04430402fe8eae3b2825b $RELOAD IndraABM.pythonanywhere.com
 touch reboot
