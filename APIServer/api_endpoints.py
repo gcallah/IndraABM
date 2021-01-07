5c52243e41d234e99219bf0e28b274ec45422c3a
Denys Fenchenko <denisfench@gmail.com>
2021-01-07 12:54:22 -0500

    changing indra_dir in api_endpoints file

--- APIServer/api_endpoints.py
+++ APIServer/api_endpoints.py
@@ -18,7 +18,7 @@ api = Api(app)
 
 # the hard-coded dir is needed for Python Anywhere, until
 # we figure out how to get the env var set there.
-indra_dir = os.getenv("INDRA_HOME", "/home/IndraABM/IndraABM")
+indra_dir = os.getenv("INDRA_HOME", "")
 
 
 @api.route('/hello')
