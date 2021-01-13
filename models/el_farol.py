2f0d1c1beeab28b912777bf72d3caa3544e2fb43
gcallah <gcallah@mac.com>
2021-01-13 17:44:32 -0500

    Chagning el_farol to use pct recently full.

--- models/el_farol.py
+++ models/el_farol.py
@@ -22,9 +22,12 @@ BAR_ATTEND = "bar attendees"
 DISC_AMT = .01
 MIN_MOTIV = 0.05
 MAX_MOTIV = .95
+HALF_FULL = .5
 DEBUG = False
 OPT_OCUPANCY = 0.6
-MEM_CAPACITY = 7  # Must be an integer
+MEMORY = 'memory'
+DEF_MEM_CAPACITY = 7  # Must be an integer
+mem_capacity = DEF_MEM_CAPACITY
 
 
 def get_decision(agent):
@@ -45,7 +48,7 @@ def discourage(agent):
 
 def encourage(agent):
     """
-    inrease motivation and try to change decision
+    Increase motivation and try to change decision.
     """
     agent[MOTIV] = min(agent[MOTIV] + DISC_AMT, MAX_MOTIV)
     encouraged = get_decision(agent)
@@ -54,16 +57,16 @@ def encourage(agent):
 
 def memory_check(agent):
     """
-    return whether bar was full on average a set day
+    Return percentage of capacity the bar was at.
     """
     bar = get_model(agent.exec_key)
-    attandance = bar.env.pop_hist.pops[AT_BAR]
+    attendance = bar.env.pop_hist.pops[AT_BAR]
     population = (bar.grp_struct[AT_BAR]["num_mbrs"] +
                   bar.grp_struct[AT_HOME]["num_mbrs"])
-    if len(attandance) < MEM_CAPACITY:
-        n_last_days = attandance
+    if len(attendance) < mem_capacity:
+        n_last_days = attendance
     else:
-        n_last_days = attandance[-MEM_CAPACITY:]
+        n_last_days = attendance[-mem_capacity:]
     average = sum(n_last_days) / len(n_last_days)
     was_full = average >= int(population * OPT_OCUPANCY)
     return was_full
@@ -71,29 +74,30 @@ def memory_check(agent):
 
 def drinker_action(agent, **kwargs):
     """
-    To go or not to go, that is the question -Not Callahan
-    for the now the decision is made at random
+    To go or not to go, that is the question.
+    The decision is based on the agent's memory of how crowded the
+    bar has been recently (a parameter).
     """
     if DEBUG:
         print("Alcoholic {} is located at {}".format(agent.name,
                                                      agent.get_pos()))
     bar = get_model(agent.exec_key)
-    was_full = memory_check(agent)
+    recently_full = memory_check(agent)
     going = get_decision(agent)
     # for people at home
     if agent.group_name() == AT_HOME:
         # if last night was full, discourage going, else encourage
-        if going and not was_full:
+        if going and not recently_full:
             # drinker has motivation to go to bar today, consider him gone
             bar.add_switch(str(agent), AT_HOME, AT_BAR)
-        elif not going and not was_full:
+        elif not going and not recently_full:
             encouraged = encourage(agent)
             if encouraged:
                 bar.add_switch(str(agent), AT_HOME, AT_BAR)
     else:
         # for people that went yesterday
         # if last night was full, discourage going, else encourage
-        if going and was_full:
+        if going and recently_full:
             # drinker has motivation to go toleave bar today, consider him gone
             discouraged = discourage(agent)
             if discouraged:
@@ -109,7 +113,9 @@ def create_drinker(name, i, exec_key=None, action=drinker_action):
     drinkers starts with a random motivation
     """
     rand_motive = random.random()
-    return Agent(name + str(i), attrs={MOTIV: rand_motive},
+    recent_crowds = [HALF_FULL]*mem_capacity
+    return Agent(name + str(i),
+                 attrs={MOTIV: rand_motive, MEMORY: recent_crowds},
                  action=action, exec_key=exec_key)
 
 
@@ -144,7 +150,8 @@ class ElFarol(Model):
         at_home = num_mbrs - at_bar
         self.grp_struct[AT_BAR]["num_mbrs"] = at_bar
         self.grp_struct[AT_HOME]["num_mbrs"] = at_home
-        # MEM_CAPACITY = self.props.get("memory")
+        global mem_capacity
+        mem_capacity = self.props.get("memory", DEF_MEM_CAPACITY)
 
 
 def create_model(serial_obj=None, props=None):
