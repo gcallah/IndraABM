924102b81e47cf7878de5c6639b5394a420b34e3
Nhi Pham <npl243@nyu.edu>
2021-01-16 01:41:05 +0400

    edgeworthbox to V3 (all values are default), fixed flake8 errors

--- capital/edgeworthbox.py
+++ capital/edgeworthbox.py
@@ -5,9 +5,10 @@ and just sets up a couple of agents in two groups that
 do nothing except move around randomly.
 """
 
-from lib.agent import MOVE
+from lib.agent import Agent, MOVE
 from lib.display_methods import RED, BLUE
-from lib.model import Model, NUM_MBRS, MBR_ACTION, NUM_MBRS_PROP, COLOR
+from lib.model import Model, NUM_MBRS, MBR_CREATOR, MBR_ACTION, COLOR
+from capital.trade_utils import GEN_UTIL_FUNC, UTIL_FUNC, AMT_AVAIL
 from capital.trade_utils import seek_a_trade
 
 DEBUG = False
@@ -15,6 +16,60 @@ DEBUG = False
 MODEL_NAME = "edgeworthbox"
 DEF_WINE_MBRS = 1
 DEF_CHEESE_MBRS = 1
+DEF_NUM_CHEESE = 4
+DEF_NUM_WINE = 4
+
+GOODS = "goods"
+INCR = "incr"
+
+
+# def create_trader(name, i, action=None, **kwargs):
+#     """
+#     A func to create a trader.
+#     """
+#     return Agent(name + str(i),
+#                  action=action,
+#                  attrs={GOODS: {},
+#                         "util": 0,
+#                         "pre_trade_util": 0},
+#                  **kwargs)
+
+def create_wine(name, i, action=None, **kwargs):
+    start_wine = DEF_NUM_WINE
+    # if props is not None:
+    #     start_wine = props.get('start_wine',
+    #                            DEF_NUM_WINE)
+    return Agent(name + str(i),
+                 action=seek_a_trade,
+                 attrs={GOODS: {"wine": {AMT_AVAIL: start_wine,
+                                         UTIL_FUNC: GEN_UTIL_FUNC,
+                                         INCR: 0},
+                                "cheese": {AMT_AVAIL: 0,
+                                           UTIL_FUNC: GEN_UTIL_FUNC,
+                                           INCR: 0}},
+                        "util": 0,
+                        "pre_trade_util": 0,
+                        "trades_with": "Cheese holders"},
+                 **kwargs)
+
+
+def create_cheese(name, i, action=None, **kwargs):
+    start_cheese = DEF_NUM_CHEESE
+    # if props is not None:
+    #     start_cheese = props.get('start_cheese',
+    #                              DEF_NUM_CHEESE)
+    return Agent(name + str(i),
+                 action=seek_a_trade,
+                 attrs={GOODS: {"cheese": {AMT_AVAIL: start_cheese,
+                                           UTIL_FUNC: GEN_UTIL_FUNC,
+                                           INCR: 0},
+                                "wine": {AMT_AVAIL: 0,
+                                         UTIL_FUNC: GEN_UTIL_FUNC,
+                                         INCR: 0}},
+                        "util": 0,
+                        "pre_trade_util": 0,
+                        "trades_with": "Wine holders"},
+                 **kwargs)
 
 
 def trader_action(agent, **kwargs):
@@ -50,6 +105,12 @@ class EdgeworthBox(Model):
     the system as a whole is working.
     It turns out that so far, we don't really need to subclass anything!
     """
+    def handle_props(self, props, model_dir=None):
+        super().handle_props(props, model_dir='capital')
+
+    def create_groups(self):
+        grps = super().create_groups()
+        return grps
 
 
 def create_model(serial_obj=None, props=None):
