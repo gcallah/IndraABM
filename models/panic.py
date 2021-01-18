1892625500674141171aa1d64c543066a202ca78
Denys Fenchenko <denisfench@gmail.com>
2021-01-18 15:25:30 -0500

    Fixing tests in panic

--- models/panic.py
+++ models/panic.py
@@ -13,7 +13,7 @@ DEBUG = False  # turns debugging code on or off
 DEBUG2 = False  # turns deeper debugging code on or off
 
 DEF_DIM = 10
-OFFSET = 25
+OFFSET = 50
 DEF_NUM_PEOPLE = DEF_DIM*DEF_DIM
 DEF_NUM_PANIC = int(.3 * DEF_NUM_PEOPLE)
 DEF_NUM_CALM = int(.7 * DEF_NUM_PEOPLE)
