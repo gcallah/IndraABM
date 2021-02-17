ca84824a5839e61dc1f2e4d7c64bc8cf24199820
Barkin Simsek <barkin@nyu.edu>
2021-02-17 13:11:21 +0400

    remove unused imports in fashion model

--- models/fashion.py
+++ models/fashion.py
@@ -2,9 +2,11 @@
 This is the Adam Smith fashion model.
 """
 
-from lib.agent import MOVE
+# from lib.agent import MOVE
 from lib.display_methods import BLUE, DARKRED, NAVY, RED
 from lib.model import COLOR, MBR_ACTION, NUM_MBRS, NUM_MBRS_PROP, Model
+
+# from registry.registry import get_model
 
 DEBUG = False
 
@@ -13,47 +15,51 @@ DEF_NUM_TSETTERS = 5
 DEF_NUM_FOLLOWERS = 55
 
 
-def tsetter_action(agent, **kwargs):
+def fashion_action(agent, **kwargs):
     """
-    A simple default agent action for trend setters
-    """
-    if DEBUG:
-        print("Agent {} is located at {}".format(agent.name,
-                                                 agent.get_pos()))
-    return MOVE
-
-
-def follower_action(agent, **kwargs):
-    """
-    A simple default agent action for followers
+    A simple default agent action
     """
     if DEBUG:
-        print("Agent {} is located at {}".format(agent.name,
-                                                 agent.get_pos()))
-    return MOVE
+        print("Agent", str(agent), "is acting.")
+
+    # model = get_model(agent.exec_key)
+    # num_others_red = len(others_red.subset(in_hood, agent, HOOD_SIZE))
+    # num_others_blue = len(others_blue.subset(in_hood, agent, HOOD_SIZE))
+    # total_others = num_others_red + num_others_blue
+    # if total_others <= 0:
+    #     return False
+
+    # env_color = ratio_to_sin(num_others_red / total_others)
+
+    # agent[COLOR_PREF] = new_color_pref(agent[COLOR_PREF], env_color)
+    # if env_unfavorable(agent[DISPLAY_COLOR], agent[COLOR_PREF], op1, op2):
+    #     change_color(agent, get_env(execution_key=execution_key), opp_group)
+    #     return True
+    # else:
+    #     return False
 
 
 fashion_grps = {
     "Blue Trendsetters": {
-        MBR_ACTION: tsetter_action,
+        MBR_ACTION: fashion_action,
         NUM_MBRS: DEF_NUM_TSETTERS,
         NUM_MBRS_PROP: "num_tsetters",
         COLOR: NAVY
     },
     "Red Trendsetters": {
-        MBR_ACTION: tsetter_action,
+        MBR_ACTION: fashion_action,
         NUM_MBRS: DEF_NUM_TSETTERS,
         NUM_MBRS_PROP: "num_tsetters",
         COLOR: DARKRED
     },
     "Blue Followers": {
-        MBR_ACTION: follower_action,
+        MBR_ACTION: fashion_action,
         NUM_MBRS: DEF_NUM_FOLLOWERS,
         NUM_MBRS_PROP: "num_followers",
         COLOR: BLUE
     },
     "Red Followers": {
-        MBR_ACTION: follower_action,
+        MBR_ACTION: fashion_action,
         NUM_MBRS: DEF_NUM_FOLLOWERS,
         NUM_MBRS_PROP: "num_followers",
         COLOR: RED
